26398afb03cc56aef5597174a63757dd99fec4e4
forever-Liudawang <2387498813@qq.com>
2019-05-17 22:33:33 +0800

    'change'

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 console.log('test1');
 console.log('test2');
 console.log('test3');
-<<<<<<< HEAD
 console.log('test4');
 var a=10;
-=======
 console.log('testtest=====');
 console.log('+++++====')
->>>>>>> dev
